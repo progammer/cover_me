26041f9d31a3fd239c811a6bd5d6bc2d43d2dc01
helen <helen524@gmail.com>
2020-10-24 07:12:12 +0000

    fixes

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,5 +1,5 @@
 function serverExample() {
-    fetch('/servlet').then((response) => response.json())
+    fetch('/test').then((response) => response.json())
       .then(function(responseJson) {
         // do something with json output of html response
       });
@@ -7,7 +7,7 @@ function serverExample() {
 
       const params = new URLSearchParams();
       params.append('param', 'test'); // can add parameters to html request
-      fetch(new Request('/servlet', {method: 'POST', body: params}))
+      fetch(new Request('/test', {method: 'POST', body: params}))
           .then(function(responseText) {
         // do something with text output of html response
       });
