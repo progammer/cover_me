21bd4f2492e3ac2664da908335963f903ba16562
fang-helen <helen524@gmail.com>
2020-10-25 09:34:44 +0000

    suppress warnings

--- src/main/java/com/google/sps/servlets/SearchServlet.java
+++ src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
 
